229d760176e18763dfec5408ba4709f8ba0ee14f
ase1590 <ase1590@hotmail.com>
2016-10-30 23:20:13 -0500

    first commit, working basic functions

---
+++ gifprocess.py
@@ -0,0 +1,32 @@
+#import urllib3
+import requests
+import os
+import time
+import sys
+
+fname = 'NatLoop.gif'
+
+os.chdir(os.path.dirname(sys.argv[0])) #sets working directory to script location
+def pullgif():
+    weathermap='http://radar.weather.gov/ridge/Conus/Loop/NatLoop.gif'
+    r = requests.get(weathermap)
+    open(fname, 'wb').write(r.content)
+
+def updategif():
+    u = os.path.getmtime(fname)
+    agediff = 900   #age difference of file in seconds
+    if time.time() > u + agediff:
+        pullgif()
+        print("updated gif")
+    else:
+        print("nothing to update")
+
+def check_exist():
+    if os.path.isfile(fname) == True:
+        updategif()
+        print("it existed, checking update")
+    else:
+        pullgif()
+        print("didnt exist, pulling")
+
+check_exist()
