43f9e4c04208488b7d5874f8e8152e3130a096d2
ase1590 <ase1590@hotmail.com>
2018-04-03 18:59:35 -0500

    added working Qt5 gif shower

---
+++ qt5gifshower.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial 
+
+In this example, we display the x and y 
+coordinates of a mouse pointer in a label widget.
+
+Author: Jan Bodnar
+Website: zetcode.com 
+Last edited: August 2017
+"""
+
+import sys
+from PyQt5.QtCore import Qt, QByteArray
+from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QLabel, QSizePolicy, QVBoxLayout
+from PyQt5.QtGui import QMovie
+import sys
+
+class ImagePlayer(QWidget):
+    def __init__(self, filename, title, parent=None):
+        QWidget.__init__(self, parent)
+
+        # Load the file into a QMovie
+        self.movie = QMovie(filename, QByteArray(), self)
+
+        size = self.movie.scaledSize()
+        self.setGeometry(200, 200, size.width(), size.height())
+        self.setWindowTitle(title)
+
+        self.movie_screen = QLabel()
+        # Make label fit the gif
+        self.movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.movie_screen.setAlignment(Qt.AlignCenter)
+
+        # Create the layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.movie_screen)
+
+        self.setLayout(main_layout)
+
+        # Add the QMovie object to the label
+        self.movie.setCacheMode(QMovie.CacheAll)
+        self.movie.setSpeed(100)
+        self.movie_screen.setMovie(self.movie)
+        self.movie.start()
+        self.movie.loopCount()
+
+
+if __name__ == "__main__":
+    gif = "regionmap.gif"
+    app = QApplication(sys.argv)
+    player = ImagePlayer(gif, "was")
+    player.show()
+    sys.exit(app.exec_())
